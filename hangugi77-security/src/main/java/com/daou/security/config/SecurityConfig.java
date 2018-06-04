ac604bd507a2c938c677e0d813a08a3bbfe42c18
mskw <mskw@daou.co.kr>
2018-06-04 17:28:27 +0900

    commit

--- hangugi77-security/src/main/java/com/daou/security/config/SecurityConfig.java
+++ hangugi77-security/src/main/java/com/daou/security/config/SecurityConfig.java
@@ -1,44 +1,37 @@
-package com.daou.security.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//package com.daou.security.config;
 
 // https://stackoverflow.com/questions/24916894/serving-static-web-resources-in-spring-boot-spring-security-application
 
-@EnableWebSecurity
+//@EnableWebSecurity
 //@EnableGlobalMethodSecurity
 //@EnableGlobalAuthentication
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	http://java.ihoney.pe.kr/498
 //	@Bean
 //	public static PasswordEncoder passwordEncoder() {
 //		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 //	}
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
-				.authorizeRequests()
-					.antMatchers("/css/**", "/index").permitAll()
-					.antMatchers("/user/**").hasRole("USER")
-				.and()
-				.formLogin()
-					.loginPage("/login")
-					.failureUrl("/login-error");
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.inMemoryAuthentication().withUser("mskw").password("{noop}ffff").roles("USER");
-	}
+//	@Override
+//	protected void configure(HttpSecurity httpSecurity) throws Exception {
+//		httpSecurity
+//				.authorizeRequests()
+//					.antMatchers("/css/**", "/index").permitAll()
+//					.antMatchers("/user/**").hasRole("USER")
+//				.and()
+//				.formLogin()
+//					.loginPage("/login")
+//					.failureUrl("/login-error");
+//	}
+//
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//		authenticationManagerBuilder.inMemoryAuthentication().withUser("mskw").password("{noop}ffff").roles("USER");
+//	}
 
 //	@Autowired
 //	public  void confgiureGlobal2(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 //		authenticationManagerBuilder.jdbcAuthentication().dataSource().withUser()
 //	}
-}
+//}
