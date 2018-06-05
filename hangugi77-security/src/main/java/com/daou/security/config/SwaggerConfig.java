51c15bbb07b7c3297ed62052734b66492b32cfbc
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2018-06-06 00:44:14 +0900

    commit!!!

--- hangugi77-security/src/main/java/com/daou/security/config/SwaggerConfig.java
+++ hangugi77-security/src/main/java/com/daou/security/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.daou.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -18,5 +20,10 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.basePackage("com.daou"))
 				.paths(PathSelectors.any())
 				.build();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**", "/h2/**");
 	}
 }
---
+++ hangugi77-security/src/main/resources/import.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE mskw;
+
+-- DROP TABLE TEST333;
+-- CREATE TABLE USERS(id int, name varying, email varying );
+
+-- INSERT INTO USERS(ID, NAME, EMAIL) VALUES (1, '문재인', 'moon@daou.com');
+-- INSERT INTO USERS(ID, NAME, EMAIL) VALUES (2, '김정은', 'kim@daou.com');
+-- INSERT INTO USERS(ID, NAME, EMAIL) VALUES (3, '트럼프', 't@daou.com');
+-- INSERT INTO USERS(ID, NAME, EMAIL) VALUES (4, '시진핑', 'xi@daou.com');
+INSERT INTO USERS(NAME, EMAIL) VALUES ('문재인', 'moon@daou.com');
+INSERT INTO USERS(NAME, EMAIL) VALUES ('김정은', 'kim@daou.com');
+INSERT INTO USERS(NAME, EMAIL) VALUES ('트럼프', 't@daou.com');
+INSERT INTO USERS(NAME, EMAIL) VALUES ('시진핑', 'xi@daou.com');
+--
+-- INSERT INTO USER_ADDRESS(ID, COUNTRY, ADDRESS, USERS_ID) VALUES (1, '한국', '서울', 1);
+-- INSERT INTO USER_ADDRESS(ID, COUNTRY, ADDRESS, USERS_ID) VALUES (2, '북한', '평양', 2);
