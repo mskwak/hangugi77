668b9ce2c3bebd7c50bae3d21d06e97eb12143c7
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2018-06-27 22:58:04 +0900

    commit!!!

--- hangugi77-tobytv10/src/main/java/com/daou/tobytv10/Tobytv10Application.java
+++ hangugi77-tobytv10/src/main/java/com/daou/tobytv10/Tobytv10Application.java
@@ -2,9 +2,18 @@ package com.daou.tobytv10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Tobytv10Application {
+    @RestController
+    public static class MyController {
+        @GetMapping("/rest")
+        public String rest() {
+            return "rest";
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Tobytv10Application.class, args);
