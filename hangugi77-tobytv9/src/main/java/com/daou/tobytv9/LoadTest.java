5afb9e77a01fda33b74df80d0ecaa3decd6f9d3c
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2018-06-27 22:55:49 +0900

    commit!!!

---
+++ hangugi77-tobytv9/src/main/java/com/daou/tobytv9/LoadTest.java
@@ -0,0 +1,44 @@
+package com.daou.tobytv10.nine;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class LoadTest {
+    static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/rest";
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        for(int i = 0; i < 100; i++) {
+            executorService.execute(() -> {
+                int idx = counter.addAndGet(1);
+                log.info("Thread {}", idx);
+
+                StopWatch inner = new StopWatch();
+                inner.start();
+
+                restTemplate.getForObject(url, String.class);
+
+                inner.stop();
+                log.info("Elapsed: {} {}", idx, inner.getTotalTimeSeconds());
+            });
+        }
+
+        stopWatch.stop();
+
+        executorService.shutdown();
+        executorService.awaitTermination(100, TimeUnit.SECONDS);
+    }
+}
