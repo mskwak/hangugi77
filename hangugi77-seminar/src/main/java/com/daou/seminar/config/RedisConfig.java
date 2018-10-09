0b507a7b9daaf82cc9294631f75f7e0862f720ce
mskw <mskw@daou.co.kr>
2018-10-09 13:31:51 +0900

    commit!!!

--- hangugi77-seminar/src/main/java/com/daou/seminar/config/RedisConfig.java
+++ hangugi77-seminar/src/main/java/com/daou/seminar/config/RedisConfig.java
@@ -1,25 +1,8 @@
-//package com.daou.seminar.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.ReactiveRedisTemplate;
-//import org.springframework.data.redis.serializer.RedisSerializationContext;
-//
-//@Configuration
-//public class RedisConfig {
-//	@Autowired
-//	private ReactiveRedisConnectionFactory redisConnectionFactory;
-//
-//	@Bean
-//	public LettuceConnectionFactory lettuceConnectionFactory()  {
-//		return new LettuceConnectionFactory();
-//	}
-//
-//	@Bean
-//	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-//		return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, RedisSerializationContext.string());
-//	}
-//}
+package com.daou.seminar.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+}
